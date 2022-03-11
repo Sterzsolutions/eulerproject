f88879bb0c8945fa7ff411512a693cc6ffdfba13
Gabriel Sterz <g.sterz.cologne@gmail.com>
2022-03-11 16:12:51 +0100

    second Problem solved

--- src/de/sterzsolutions/one/MultiplesOfThreeOrFive.java
+++ src/de/sterzsolutions/one/MultiplesOfThreeOrFive.java
@@ -20,7 +20,7 @@ public class MultiplesOfThreeOrFive {
 
         for (int i = 0; i < 1000; i++){
             if((i % 3 == 0) || (i % 5 == 0)){
-                solution = solution + i;
+                solution += i;
             }
         }
 
